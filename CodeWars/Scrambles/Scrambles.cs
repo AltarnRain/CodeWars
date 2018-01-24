0018bbdec2d8f3db555c3189c91f030899e77f22
Onno <mabusraeen@gmail.com>
2018-01-24 22:45:56 +0100

    Scramble

--- CodeWars/Scrambles/Scrambles.cs
+++ CodeWars/Scrambles/Scrambles.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Linq;
+
 public class Scramblies
 {
 
     public static bool Scramble(string str1, string str2)
     {
-        // your code
-        // Mark as edited
+        var scambled = from c in str1.Distinct()
+                     select new { c, count = str1.Count(c2 => c2 == c) };
 
-        return false;
-    }
+        var sorted = from c in str2.Distinct()
+                        select new { c, count = str2.Count(c2 => c2 == c) };
+
+
+        return sorted.All(x => x.c ) 
+        }
 
 }
