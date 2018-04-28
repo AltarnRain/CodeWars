b8984a59583455901584d42654d62914347da286
Onno <mabusraeen@gmail.com>
2018-04-28 12:46:19 +0200

    Finished PlayingWithDigits

---
+++ CodeWars/CountCharactersInString/CountCharactersInString.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.CountCharactersInString
+{
+    public class Kata
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            return str.Distinct().ToDictionary(p => p, p => str.Count(s => s == p));
+        }
+    }
+}
---
+++ CodeWars/PlayingWithDigits/PlayingWithDigits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.PlayingWithDigits
+{
+    public class DigPow
+    {
+        public static long digPow(int n, int p)
+        {
+            var sumOfPowers = n.ToString().Select(s => int.Parse(s.ToString())).Select((d, i) => Math.Pow(d, p + i)).Sum();
+            return sumOfPowers % n == 0 ? (long)sumOfPowers / n : - 1;
+        }
+    }
+}
