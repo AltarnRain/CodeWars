b8b72e6c1e2d8e2afc6a372ffb9a668535e84310
Onno <mabusraeen@gmail.com>
2018-09-30 07:43:07 +0200

    Added SquareIntoSquares

---
+++ CodeWars/SquareIntoSquares/SquareIntoSquares.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.SquareIntoSquares
+{
+    public class Decompose
+    {
+
+        public string decompose(long n)
+        {
+            var square = Math.Pow(n, 2);
+            var start = n - 1;
+            var numbers = new List<List<long>>
+            {
+                new List<long>()
+            };
+
+            bool improve = true;
+            while (improve)
+            {
+                while (square != 0 && start != 0)
+                {
+                    if (square - Math.Pow(start, 2) >= 0)
+                    {
+                        numbers.Last().Add(start);
+                        square -= Math.Pow(start, 2);
+                    }
+
+                    start--;
+                }
+            }
+
+            numbers.Sort();
+
+            return string.Join(" ", numbers);
+        }
+    }
+}
---
+++ CodeWars/SquareIntoSquares/SquareIntoSquaresTests.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeWars.SquareIntoSquares
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DecomposeTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Decompose d = new Decompose();
+            long n = 11;
+            Assert.AreEqual("1 2 4 10", d.decompose(n));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Decompose d = new Decompose();
+            long n = 50;
+            Assert.AreEqual("1 3 5 8 49", d.decompose(n));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Decompose d = new Decompose();
+            long n = 49;
+            Assert.AreEqual(null, d.decompose(n));
+        }
+    }
+
+}
