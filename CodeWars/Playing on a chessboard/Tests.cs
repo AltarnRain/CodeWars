71e63627d8c17be514af2dd468cd8b5ecc1a908e
Onno <onno.invernizzi@ivention.nl>
2021-11-20 11:40:38 +0100

    Setup new kata.

---
+++ CodeWars/Playing on a chessboard/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace CodeWars.Playingonachessboard
+{
+    [TestFixture]
+    public class Suite2Tests
+    {
+
+        Random randomGenerator = new Random();
+
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual("[0]", Suite2.game(0));
+        }
+        
+        [Test]
+        public void Test02()
+        {
+            Assert.AreEqual("[1, 2]", Suite2.game(1));
+        }
+
+        [Test]
+        public void Test03()
+        {
+            Assert.AreEqual("[32]", Suite2.game(8));
+        }
+    }
+}
+
